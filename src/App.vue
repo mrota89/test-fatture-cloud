9c0592e5a1d5e9fe75c1a79416ad5f32f4145911
mrota89 <marcello.rota.1989@gmail.com>
2021-05-18 11:23:32 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Month v-for="(mese, index) in month" :month="mese" :key="index"/>
+  </div>
+</template>
+
+<script>
+import Month from './components/Month.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Month,
+  },
+  data: function(){
+    return {
+      month: 
+      [
+        'Gennaio', 
+        'Febbraio', 
+        'Marzo', 
+        'Aprile', 
+        'Maggio', 
+        'Giugno', 
+        'Luglio', 
+        'Agosto', 
+        'Settembre', 
+        'Ottobre', 
+        'Novembre', 
+        'Dicembre'
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Month.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="frame">
+    <div class="month">{{ month }}</div>
+    <div class="total">
+      <div class="n-bill"></div>
+      <div class="revenue"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Month',
+  props: {
+    month: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
